b23bf463cfdba4b4668ebe3d85245f1a9114bb9f
biohazards1108-lab <biohazards1108@gmail.com>
2026-06-29 21:46:54 -0400

    Create config.php

---
+++ backend/config.php
@@ -0,0 +1,10 @@
+<?php
+// Copy to config.php and fill in real values.
+return [
+    'db_host' => '127.0.0.1',
+    'db_user' => 'darksbishop1109',
+    'db_pass' => 'worldofwarcraftt',
+    'db_name_auth' => 'Acore_world_auth',      // WoW auth DB
+    'db_name_website' => 'darks_wows_site', // website stats/security DB
+    'admin_secret' => 'DarkBishop0709$', // used for hidden stats page
+];
